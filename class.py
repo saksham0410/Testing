61b4c7afdc143074c83fd8e2066e3373a898f178
saksham.indurkhya@wipro.com <saksham.indurkhya@wipro.com>
2022-01-02 14:53:29 +0530

    class and gitignore file

---
+++ class.py
@@ -0,0 +1,15 @@
+class Parent:
+
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def print_value(self):
+        print(f"My name is {self.a} and age is {self.b}")
+
+    def work(self):
+        print("This is a work function")
+
+
+obj = Parent("Saksham",30)
+obj.print_value()
